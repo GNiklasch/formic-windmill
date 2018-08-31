10fa7dea629c58559753a15f2ad22f457c3a4926
GNiklasch <32454234+GNiklasch@users.noreply.github.com>
2018-08-31 09:45:00 +0200

    v2.1 release candidate build

--- docs/Windmill.js
+++ docs/Windmill.js
@@ -1,5 +1,5 @@
 /*
- * GNi 2017-09-27 - 2018-06-05
+ * GNi 2017-09-27 - 2018-08-31
 
 For background and context, see:
 https://codegolf.stackexchange.com/questions/135102/formic-functions-ant-queen-of-the-hill-contest
